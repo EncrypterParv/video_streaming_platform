85ef6717884d962c79ebc0adb515a5432b176dc7
EncrypterParv <parvgoyal1321@gmail.com>
2026-06-08 15:30:27 +0530

    connect mongodb atlas

---
+++ src/db/index_db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import {DB_NAME} from "../constants.js";
+
+const connectDB= async function(){
+    try{
+        const connectionInstance= await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n Mongodb connected to DB Host : ${connectionInstance.connection.host}`); 
+
+    }catch(error){
+        console.error(error);
+        process.exit(1);
+    }
+}
+export default connectDB;
---
+++ src/index.js
@@ -0,0 +1,44 @@
+import 'dotenv/config';
+import connectDB from './db/index_db.js';
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import mongoose from "mongoose";
+import {DB_NAME} from "./constants";
+import express from "express";
+const app= express()
+;(async ()=>{
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("errror", (error)=>{
+            console.log("there is an erro connecting your database")
+            throw error
+        });
+        app.listen(process.env.PORT, ()=>{
+            console.log("App is listening on port :", process.env.PORT )
+        })
+    }catch(error){
+        console.log("ERROR: ",error);
+        throw err
+    }
+})()
+*/
