f564a5dd6afb9f234dc13634a04fca77d6e53c9a
EncrypterParv <parvgoyal1321@gmail.com>
2026-06-10 16:21:41 +0530

    utils changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import 'dotenv/config';
 import connectDB from './db/index_db.js';
 
 connectDB()
-
+.then(()=>{
+    app.listen(process.env.PORT || 1321, ()=>{
+        console.log(`server is running at port ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("Mongodb connection failed!!!", err)
+})
 
 
 
